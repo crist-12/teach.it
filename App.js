49702ade2a95a3f0dc934ea258aac947cd64f6f8
Lizzie Maradiaga <lg_castrom@unicah.edu>
2021-03-10 16:54:53 -0600

    Cambios en App con pantalla de ResetPassword y envio de parametros iniciales, en pantalla Principal se añade que aparezca el nombre del usuario logueado y al salir se resetean valores de password restablecida o usuario creado

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet } from 'react-native';
 import { ThemeProvider } from "react-native-elements";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -7,26 +7,36 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "./src/components/screens/SignIn";
 import SignUp from "./src/components/screens/SignUp";
 import Principal from "./src/components/screens/Principal";
-import Discover from "./src/components/screens/Discover";
-import Profile from './src/components/screens/Profile'
-import CreditCard from './src/components/screens/CreditCard'
+import CreditCard from './src/components/screens/CreditCard';
 import theme from "./src/theme";
+import ResetPassword from "./src/components/screens/ResetPassword";
+import persistLogin from "./src/firebase/persistLogin";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState({});
+
+  // Verificar si ya existen credenciales de autenticación
+  useEffect(() => {
+    const userData = persistLogin();
+    setUser(userData);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{headerShown: false}}>
-         
+            <Stack.Screen name="SignIn" component={SignIn} initialParams={{ userCreated:false, passwordReset: false }}/>
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="ResetPassword" component={ResetPassword} />
+            <Stack.Screen name="Principal" component={Principal} initialParams={{ user:false }}/>
             <Stack.Screen name="CreditCard" component={CreditCard} />
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
     </ThemeProvider>
-
   );
 }
 
--- src/components/screens/Principal.js
+++ src/components/screens/Principal.js
@@ -1,124 +1,146 @@
-import React from "react";
-import { StyleSheet, Dimensions, View, StatusBar, ScrollView, ImageBackground} from "react-native";
-import { Text, Icon, Button,Input,Header} from "react-native-elements";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { Text, Icon, Input } from "react-native-elements";
 import CardForm from "../forms/CardForm";
-
+import { firebase } from "../../firebase";
+import Alert from "../shared/Alert";
 
 const { width, height } = Dimensions.get("window");
 
-const Principal = ({navigation}) => {
+const Principal = ({ navigation, route }) => {
+  const { user } = route.params;
+  const [error, setError] = useState(false);
 
-    
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        console.log("Sesion Cerrada");
+        navigation.navigate("SignIn", { userCreated: false });
+      })
+      .catch((error) => {
+        // An error happened.
+        setError("Ocurrió un error al intentar cerrar sesión");
+      });
+  };
+
   return (
     <View style={styles.container}>
-      
       <StatusBar barStyle="light-content" />
-      
+
       <View style={styles.head}>
-      <View style={styles.buscar}>
-      <Input
-            placeholder="Buscar" placeholderTextColor="#fff" 
+        <TouchableOpacity onPress={signOut} style={styles.logout}>
+          <Icon
+            name="logout"
+            color="#fff"
+            style={{ transform: [{ rotateY: "180deg" }] }}
+          />
+        </TouchableOpacity>
+        <View style={styles.buscar}>
+          <Input
+            placeholder="Buscar"
+            placeholderTextColor="#fff"
             rightIcon={{
               type: "font-awesome",
               name: "search",
               color: "#fff",
               style: { marginRight: 12 },
-              
             }}
-           
           />
-      
-          
-            </View>
-      </View >
+        </View>
+      </View>
       <View style={styles.titulo}>
-        <Text  style={styles.txtTitulo}>Tutorías</Text>
+        <Text style={styles.txtTitulo}>{user.fullname}</Text>
+        <Text style={styles.txtTitulo}>Tutorías</Text>
       </View>
-     
-      <ScrollView style={{width:width,paddingLeft:width*0.05}}>
-        
-        <CardForm
 
-        clases="Musica"
-        tutor="Benito Martinez"
-        hora="15:00"
-        tutoria="1"
-        disponible="1"
+      <ScrollView style={{ width: width, paddingLeft: width * 0.05 }}>
+        {error ? <Alert title={error} type="error" /> : null}
+        <CardForm
+          clases="Musica"
+          tutor="Benito Martinez"
+          hora="15:00"
+          tutoria="1"
+          disponible="1"
         />
         <CardForm
-
-        clases="Filosofia"
-        tutor="Armando hoyos"
-        hora="16:00"
-        tutoria="1"
+          clases="Filosofia"
+          tutor="Armando hoyos"
+          hora="16:00"
+          tutoria="1"
         />
         <CardForm
-
-        clases="Programacion"
-        tutor="Thomas A. Anderson"
-        hora="17:00"
-        tutoria="1"
+          clases="Programacion"
+          tutor="Thomas A. Anderson"
+          hora="17:00"
+          tutoria="1"
         />
-        
       </ScrollView>
-      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-        alignItems: 'center',
-      backgroundColor:"#fff"
-
-    },
-    buscar:{
-      flexDirection:"row",
-      justifyContent:"space-between",
-      width:width,
-      height:55,
-      backgroundColor:"#232425",
-      
- 
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
-  head:{
-    width:width,
+  buscar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: width,
+    height: 55,
+    backgroundColor: "#232425",
+  },
+  head: {
+    width: width,
+    flexDirection: "row",
   },
   imageBackgroundContainer: {
-    width:width,
-    height:height,
-    position:"absolute",
+    width: width,
+    height: height,
+    position: "absolute",
     alignItems: "center",
     resizeMode: "cover",
-
   },
   imageBackgroundContainer1: {
-   width:width,
+    width: width,
     alignItems: "center",
     resizeMode: "cover",
-
   },
-  titulo:{
-    width:width,
-    paddingTop:5,
-    paddingBottom:5,
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor:"#fe5722",
-    
-
+  titulo: {
+    width: width,
+    paddingTop: 5,
+    paddingBottom: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fe5722",
   },
-  txtTitulo:
-  {
-    paddingLeft:15,
-    paddingRight:15,
-   fontSize:25,
-   color:"#fff",
-   borderColor:"#fff",
-   borderWidth:0,
-   borderRadius:5,
-  }
-    
-  });
+  txtTitulo: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    fontSize: 25,
+    color: "#fff",
+    borderColor: "#fff",
+    borderWidth: 0,
+    borderRadius: 5,
+  },
+  logout: {
+    backgroundColor: "#232425",
+    paddingRight: 7,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
 
 export default Principal;
