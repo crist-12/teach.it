567df3a3e48790e024ee8e0bfc9377b1cde30d38
Lizzie Maradiaga <lg_castrom@unicah.edu>
2021-03-16 15:46:40 -0600

    Cambios en el estado al momento de verificar inicio de sesion con token existente, creacion del metodo para persistLogin y signOut

--- src/providers/AuthContext.js
+++ src/providers/AuthContext.js
@@ -1,21 +1,29 @@
 import createDataContext from "./createDataContext";
-import firebase from "../firebase";
-import persistLogin from "./firebase/persistLogin";
+import { firebase } from "../firebase";
 
 //Acciones disponibles para el reducer
 const authReducer = (state, action) => {
   switch (action.type) {
     case "errorMessage":
       return { ...state, errorMessage: action.payload };
-      break;
     case "signIn":
-      return { ...state, user: action.payload };
+      return { ...state, user: action.payload, loggedIn: true };
+    case "signOut":
+      return { ...state, user: action.payload, loggedIn: false };
+    case "persistLogin":
+      return {
+        ...state,
+        user: action.payload.user,
+        loggedIn: action.payload.loggedIn,
+        loading: false,
+      };
     default:
       return state;
   }
 };
 
-const signIn = (dispatch) => ({ email, password }) => {
+// Permite el inicio de sesión mediante firebase con email y password
+const signIn = (dispatch) => ( email, password ) => {
   //Hacer peticion al API de firebase
   firebase
     .auth()
@@ -28,8 +36,7 @@ const signIn = (dispatch) => ({ email, password }) => {
       // Obtener la colección desde Firebase
       const usersRef = firebase.firestore().collection("users");
 
-      // Verificar que el usuario existe en Firebase authentication
-      // y también está almacenado en la colección de usuarios.
+      // Verificar que el usuario existe en Firebase authentication y también está almacenado en la colección de usuarios.
       usersRef
         .doc(uid)
         .get()
@@ -40,7 +47,8 @@ const signIn = (dispatch) => ({ email, password }) => {
               payload: "¡El usuario no existe en la base de datos!",
             });
           } else {
-            //Llamar el reducer y enviarlelos valores del usuario al estado
+            //Llamar el reducer y enviarle los valores del usuario al estado
+            dispatch({ type: "errorMessage", payload: "" });
             dispatch({ type: "signIn", payload: firestoreDocument.data() });
           }
         });
@@ -50,8 +58,54 @@ const signIn = (dispatch) => ({ email, password }) => {
     });
 };
 
+// Cierra la sesión del usuario
+const signOut = (dispatch) => () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      dispatch({ type: "signOut", payload: {} });
+    })
+    .catch((error) => {
+      dispatch({ type: "errorMessage", payload: error.message });
+    });
+};
+
+// Verifica si existe el token de firebase para iniciar sesión sin credenciales
+const persistLogin = (dispatch) => () => {
+  const userRef = firebase.firestore().collection("users");
+
+  // Si el usuario ya se ha autenticado previamente, retornar la información del usuario, caso contrario,retonar un objeto vacío.
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      userRef
+        .doc(user.uid)
+        .get()
+        .then((document) => {
+          dispatch({
+            type: "persistLogin",
+            payload: { user: document.data(), loggedIn: true },
+          });
+        })
+        .catch((error) => {
+          dispatch({ type: "errorMessage", payload: error.message });
+        });
+    } else {
+      dispatch({
+        type: "persistLogin",
+        payload: { user: {}, loggedIn: false },
+      });
+    }
+  });
+};
+
+// Exportar las funcionalidades requeridas al contexto
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signIn },
-  { user: {}, errorMessage: "" }
+  {
+    signIn,
+    signOut,
+    persistLogin,
+  },
+  { user: {}, errorMessage: "", loggedIn: false, loading: true }
 );
