5565adedcc2995d8677e2fc45e46772bcac232d4
Lizzie Maradiaga <lg_castrom@unicah.edu>
2021-04-02 16:57:16 -0600

    Moviendo la navegacion de App, cambio de imagen del splashscreen

--- App.js
+++ App.js
@@ -1,50 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet } from 'react-native';
-import { ThemeProvider } from "react-native-elements";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import SignIn from "./src/components/screens/SignIn";
-import SignUp from "./src/components/screens/SignUp";
-import Principal from "./src/components/screens/Principal";
-import CreditCard from './src/components/screens/CreditCard';
+import { Provider as AuthProvider } from "./src/providers/AuthContext";
+import { ThemeProvider } from "react-native-elements";
+import Navigation from "./src/components/navigation";
+import LongTimers from "./src/utils/LongTimers";
 import theme from "./src/theme";
-import ResetPassword from "./src/components/screens/ResetPassword";
-import persistLogin from "./src/firebase/persistLogin";
-
-const Stack = createStackNavigator();
 
 export default function App() {
-  const [user, setUser] = useState({});
-
-  // Verificar si ya existen credenciales de autenticación
-  useEffect(() => {
-    const userData = persistLogin();
-    setUser(userData);
-  }, []);
+  LongTimers();
 
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="SignIn" component={SignIn} initialParams={{ userCreated:false, passwordReset: false }}/>
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="ResetPassword" component={ResetPassword} />
-            <Stack.Screen name="Principal" component={Principal} initialParams={{ user:false }}/>
-            <Stack.Screen name="CreditCard" component={CreditCard} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <Navigation />
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
